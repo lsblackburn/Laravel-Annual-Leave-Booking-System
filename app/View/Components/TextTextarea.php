5050047dbc26a70318347b0da61bf0db2430f11b
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-23 22:41:07 +0100

    Altered component to be proper PHP syntax

--- app/View/Components/TextTextarea.php
+++ app/View/Components/TextTextarea.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class form-feedback extends Component
+class TextTextarea extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class form-feedback extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.form-feedback');
+        return view('components.text-textarea');
     }
 }
