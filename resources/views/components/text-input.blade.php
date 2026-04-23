c34881d32e84cfb95573989d3df33dc6569263b8
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-23 23:24:04 +0100

    Amended text input based on review

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,12 @@
-@props(['disabled' => false])
+@props([
+    'disabled' => false,
+    'value' => null,
+])
 
-<input @disabled($disabled) value="{{ old($attributes->get('name')) }}" {{ $attributes->merge(['class' => 'bg-[--color-background] border-[--color-border] text-[--color-text] placeholder:text-[--color-surface-alt] focus:border-[--color-primary] focus:ring-[--color-primary] rounded-md shadow-sm']) }}>
+<input
+    @disabled($disabled)
+    value="{{ old($attributes->get('name'), $value) }}"
+    {{ $attributes->merge([
+        'class' => 'bg-[--color-background] border-[--color-border] text-[--color-text] placeholder:text-[--color-surface-alt] focus:border-[--color-primary] focus:ring-[--color-primary] rounded-md shadow-sm'
+    ]) }}
+>
