d8c6e578517925da525f0c28510583a3c68333d6
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-30 17:11:00 +0100

    Added full calendar to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,4 +5,13 @@
         </h2>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            <div id="calendar" class="p-6 box-border rounded-3xl"></div>
+
+        </div>
+
+    </div>
+
 </x-app-layout>
