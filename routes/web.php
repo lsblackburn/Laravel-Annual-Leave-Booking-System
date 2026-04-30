29028d004b8c0e5c1ac33913ba43da4afb5315c5
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-30 18:59:29 +0100

    Auth protected all routes except from login

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,17 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/leave-requests/calendar-events', [LeaveController::class, 'calendar_events'])->name('leave-requests.calendar-events');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
     Route::get('/calendar', function () {
         return view('calendar');
     })->name('calendar');
+
+    Route::get('/leave-requests/calendar-events', [LeaveController::class, 'calendar_events'])->name('leave-requests.calendar-events');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
