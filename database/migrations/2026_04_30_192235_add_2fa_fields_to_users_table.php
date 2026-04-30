3abd484a6907ccd7261e443ac0dc81d0003ff314
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-04-30 22:39:15 +0100

    Altered google2fa_secret column to take in a text instead of a string for larger character set

--- database/migrations/2026_04_30_192235_add_2fa_fields_to_users_table.php
+++ database/migrations/2026_04_30_192235_add_2fa_fields_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('google2fa_secret')->nullable()->after('remember_token');
+            $table->text('google2fa_secret')->nullable()->after('remember_token');
         });
     }
 
