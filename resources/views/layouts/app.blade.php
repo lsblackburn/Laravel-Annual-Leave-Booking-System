a12ec2b0f13eb6365e6d8aa1b245cdb202cddbf0
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-04 23:41:11 +0100

    Added background to bodys of layout files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="bg-[--color-background] antialiased">
 
         <x-setting/>
 
