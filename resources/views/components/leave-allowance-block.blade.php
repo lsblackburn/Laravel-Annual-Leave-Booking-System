e72fae27d47a579ab589e504f3a69c645dcfb5fa
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-13 23:41:27 +0100

    Moved leave allowance block to a component to be re-used. With new logic for pending leave to be shown

---
+++ resources/views/components/leave-allowance-block.blade.php
@@ -0,0 +1,26 @@
+<div class="max-w-7xl mx-auto py-6 px-3 sm:px-6 lg:px-8">
+    <div class="overflow-x-auto rounded-2xl border border-[var(--color-border)] bg-[var(--color-card)] shadow-sm">
+        <div class="p-4 sm:p-6 bg-[--color-card] shadow sm:rounded-lg">
+            <div class="flex flex-col sm:flex-row items-center justify-between gap-2">
+                <div>
+                    <p class="text-sm text-center sm:text-left text-[--color-subtletext]">Your Leave Allowance</p>
+                    <p
+                        class="text-xl md:text-2xl lg:text-3xl text-center sm:text-left font-semibold text-[--color-text]">
+                        {{ Auth::user()->leave_allowance }} days
+                    </p>
+                </div>
+
+                <div>
+                    <p class="text-sm text-center sm:text-right text-[--color-subtletext]">Remaining</p>
+                    <p class="text-xl text-center sm:text-right font-medium text-[--color-primary]">
+                        {{ Auth::user()->remainingLeaveAllowance() }} days
+                    </p>
+                    @if (Auth::user()->calculatePendingLeaveNumber() != 0)
+                        <p class="text-xs text-center sm:text-right text-[--color-primary]">Pending
+                            {{ Auth::user()->calculatePendingLeaveNumber() }} days</p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
