b8827485c2ad8c6261b4a4760268c33152e2f16a
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-20 15:11:16 +0100

    Added correct spacing for consistency

--- resources/views/components/dashboard-calendar.blade.php
+++ resources/views/components/dashboard-calendar.blade.php
@@ -1,4 +1,4 @@
-<div class="py-12">
+<div class="py-6">
     <div class="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8 space-y-6">
 
         <div class="bg-[--color-card] p-6 box-border rounded-lg">
--- resources/views/leave/form.blade.php
+++ resources/views/leave/form.blade.php
@@ -7,7 +7,7 @@
 
     <x-leave-allowance-block />
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8 space-y-6">
 
             <div class="p-4 sm:p-8 bg-[--color-card] shadow rounded-lg">
