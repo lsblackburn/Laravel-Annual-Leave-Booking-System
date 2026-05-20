cde3eed66e98abd700179fb68e02dccd1dafb9ba
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-20 13:45:36 +0100

    Notification comments

--- app/Notifications/LeaveRequestRespondedEmail.php
+++ app/Notifications/LeaveRequestRespondedEmail.php
@@ -4,6 +4,9 @@ namespace App\Notifications;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 
+/**
+ * Queued mail-only variant so email delivery cannot delay the admin response flow.
+ */
 class LeaveRequestRespondedEmail extends LeaveRequestResponded implements ShouldQueue
 {
     /**
--- app/Notifications/LeaveRequestSubmitted.php
+++ app/Notifications/LeaveRequestSubmitted.php
@@ -7,6 +7,9 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
+/**
+ * Synchronous in-app notification sent to admins when an employee submits leave.
+ */
 class LeaveRequestSubmitted extends Notification
 {
     use Queueable;
