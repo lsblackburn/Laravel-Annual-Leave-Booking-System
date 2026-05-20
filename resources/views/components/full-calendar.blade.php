68f2dd476776d305241501d55eb33b5a8f4994e0
Lewis Blackburn <lewisblackburn9@gmail.com>
2026-05-20 15:15:13 +0100

    rounded-lg for full calendar page

--- resources/views/components/full-calendar.blade.php
+++ resources/views/components/full-calendar.blade.php
@@ -2,7 +2,7 @@
 
     <div class="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8 space-y-6">
 
-        <div class="bg-[--color-card] p-6 box-border rounded-3xl">
+        <div class="bg-[--color-card] p-6 box-border rounded-lg">
 
             <div id="calendar"></div>
 
